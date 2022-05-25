f8f177d040293058fdcad7a86479f009c54a89b5
CaraErskine <caraerskine@gmail.com>
2022-05-25 11:33:26 -0400

    commit tree 35837bb311df6ffd0572ecd8b6a61ea871d6f154

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo" , "Otis" , "Garfield"];
+
+
+function destructivelyAppendCat(name){
+    cats.push (name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift (name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name){
+    // const cats = ["Milo" , "Otis" , "Garfield"];
+    return [...cats , name];
+}
+
+function prependCat(name) {
+    return [name , ...cats];
+}
+
+function removeLastCat(name){
+    return cats.slice(0, 2);
+}
+
+function removeFirstCat(name){
+    return cats.slice(1, 3);
+}
